c012f5ff490dc63874926d80bef5b0218313eef3
JessicaQueiroz <jehqueiroz01@gmail.com>
2021-01-26 19:53:32 -0300

    Commit.

--- app/src/main/java/com/example/caixatexto/MainActivity.java
+++ app/src/main/java/com/example/caixatexto/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.caixatexto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enviar(View view){
+        EditText et_nome = findViewById(R.id.et_nome);
+        EditText et_email = findViewById(R.id.et_email);
+        TextView tv_resultado = findViewById(R.id.tv_resultado);
+
+        String nome = et_nome.getText().toString();
+        String email = et_email.getText().toString();
+        tv_resultado.setText(nome);
     }
 }
